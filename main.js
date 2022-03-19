b4fcf1759cb9c032043cf95f28c2e5aafed1decf
DTurner94 <turnerdandre23@gmail.com>
2022-03-19 18:20:01 -0400

    udemyCourse

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
+/*
+//Values and Variables
+
+//"js is a variable and 'amazing' is an value"
+
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
+//if (js === 'amazing') alert('Javascript is FUN!');
 
 40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+//console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+
+let firstName = 'Goat';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myCountry = "USA"
+let myContinent = "North America"
+let myPopulation = "330 Million"
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(myPopulation)
+*/
+
+//DATA TYPES (Numbers,Boolean,Strings,Null,Undefined,Symbol,BigInt)
+
+//Boolean Example
+true;
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+
+// "Type Of" - Operators - Will define what data type in console
+console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
