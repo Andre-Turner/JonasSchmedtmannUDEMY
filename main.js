7e7046228ffde1ef7f5d2e95fd1b258f4fa629c2
DTurner94 <turnerdandre23@gmail.com>
2022-03-19 21:48:38 -0400

    operatorStudy

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
-//Values and Variables
+//VARIABLES AND VALUES
 
 //"js is a variable and 'amazing' is an value"
 
@@ -31,7 +31,7 @@ console.log(myPopulation)
 //DATA TYPES (Numbers,Boolean,Strings,Null,Undefined,Symbol,BigInt)
 
 //Boolean Example
-true;
+/*true;
 let JavascriptIsFun = true;
 console.log(JavascriptIsFun);
 
@@ -40,3 +40,75 @@ console.log(typeof true);
 console.log(typeof JavascriptIsFun);
 console.log(typeof 23);
 console.log(typeof 'Jonas');
+
+//Assigning new value to existing variable - Do not put let
+JavascriptIsFun = 'YES!';
+console.log(typeof JavascriptIsFun);
+
+//Undefined Example and reassigning value
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//Null Example
+console.log(typeof null);
+*/
+
+/*//Let,Const,Var
+
+// Let - variable that can be mutated (changed)
+let age = 30;
+age = 31;
+
+//Const - variable that can't be mutated
+const birthYear = 1991;
+//birthYear = 1994;
+
+// Var 
+var job = 'programmer';
+job = 'teacher'
+*/
+
+/*
+// MATH OPERATORS
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// PLUS OPERATOR - used to concatenate strings
+const firstName = 'Andre';
+const lastName = 'Turner';
+console.log(firstName + ' ' + lastName); // + ' ' + Template String which puts a space between name
+
+// ASSIGNMENT OPERATOR (=)
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *=4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// COMPARISON OPERATORS
+console.log(ageJonas > ageSarah); // >, <, >==, <==
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+
+console.log(now - 1991 > now - 2018);
